e0d66b78e291bc7d47878301ceb2a3105f009927
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-12-19 12:52:13 +0100

    Add solution for day 15 part two

--- src/main/kotlin/days/Day15.kt
+++ src/main/kotlin/days/Day15.kt
@@ -5,7 +5,7 @@ class Day15 : Day(15) {
     override fun partOne(): Any {
         val n = inputString.split(',').map { it.toInt() }.toMutableList()
 
-        for (i in n.lastIndex until 30000000 - 1) {
+        for (i in n.lastIndex until 2019) {
             if (n.singleOrNull { it == n[i] } != null) n.add(0)
             else {
                 val t = n.toIntArray().copyOfRange(0, i)
@@ -17,6 +17,14 @@ class Day15 : Day(15) {
     }
 
     override fun partTwo(): Any {
-        return 0
+        val n = inputString.split(',').mapIndexed { index, s -> s.toInt() to index }.toMap().toMutableMap()
+        var number = 0
+
+        for (i in n.size until 29999999) {
+            val last = n.put(number, i)
+            number = if (last == null) 0 else i - last
+        }
+
+        return number
     }
 }
